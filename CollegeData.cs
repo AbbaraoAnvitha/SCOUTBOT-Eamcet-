aebc10e0fe025f64cbf51543a4690c1fbaa82e75
AbbaraoAnvitha <anvitha.2697@gmail.com>
2017-06-09 10:54:54 -0400

    Scout Bot

---
+++ CollegeData.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Data;
+
+namespace Scout
+{
+
+
+    public class CollegeData
+    {
+
+        
+        
+
+        public string findBranches(string college)
+        {
+            Microsoft.Office.Interop.Excel.Application app;
+            Microsoft.Office.Interop.Excel.Workbook workBook;
+            Microsoft.Office.Interop.Excel.Worksheet workSheet;
+            DataTable dt;
+            string expression;
+            app = new Microsoft.Office.Interop.Excel.Application();
+            workBook = app.Workbooks.Open(@"C:\\Users\\Sathya\\Pictures\\SCOUT-master\\Dialogs\\eamcetdataset.xlsx", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+
+            workSheet = (Microsoft.Office.Interop.Excel.Worksheet)workBook.ActiveSheet;
+
+            int index = 0;
+            object rowIndex = 2;
+            dt = new DataTable();
+            dt.Columns.Add("College");
+            dt.Columns.Add("Branch");
+            dt.Columns.Add("ocb");
+            dt.Columns.Add("ocg");
+            /*dt.Columns.Add("scb");
+            dt.Columns.Add("scg");
+            dt.Columns.Add("stb");
+            dt.Columns.Add("bcab");
+            dt.Columns.Add("bcag");
+            dt.Columns.Add("bcbb");
+            dt.Columns.Add("bcbg");
+            dt.Columns.Add("bccb");
+            dt.Columns.Add("bccg");
+            dt.Columns.Add("bcdb");
+            dt.Columns.Add("bcdg");
+            dt.Columns.Add("bceb");
+            dt.Columns.Add("bceg");*/
+
+
+            DataRow row;
+
+            while (((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, 1]).Value2 != null)
+            {
+                rowIndex = 2 + index;
+                row = dt.NewRow();
+                row[0] = Convert.ToString(((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, 1]).Value2);
+                row[1] = Convert.ToString(((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, 2]).Value2);
+                row[2] = Convert.ToInt32(((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, 3]).Value2);
+                row[3] = Convert.ToInt32(((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, 4]).Value2);
+               /* row[4] = Convert.ToInt32(((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, 5]).Value2);
+                row[5] = Convert.ToInt32(((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, 6]).Value2);
+                row[6] = Convert.ToInt32(((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, 7]).Value2);
+                row[7] = Convert.ToInt32(((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, 8]).Value2);
+                row[8] = Convert.ToInt32(((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, 9]).Value2);
+                row[9] = Convert.ToInt32(((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, 10]).Value2);
+                row[10] = Convert.ToInt32(((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, 11]).Value2);
+                row[11] = Convert.ToInt32(((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, 12]).Value2);
+                row[12] = Convert.ToInt32(((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, 13]).Value2);
+                row[13] = Convert.ToInt32(((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, 14]).Value2);
+                row[14] = Convert.ToInt32(((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, 15]).Value2);
+                row[15] = Convert.ToInt32(((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, 16]).Value2);
+                row[16] = Convert.ToInt32(((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, 17]).Value2);
+                /*row[17] = Convert.ToInt32(((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, 18]).Value2);*/
+                index++;
+                dt.Rows.Add(row);
+            }
+            app.Workbooks.Close();
+
+            expression = "College  = 'KSHATRIYA COLLEGE OF ENGINEERING'";
+            DataRow[] foundRows;
+
+            // Use the Select method to find all rows matching the filter.
+            foundRows = dt.Select(expression);
+
+            // Print column 0 of each returned row.
+            string result = "";
+            
+            for (int i = 0; i < foundRows.Length; i++)
+            {
+                result += foundRows[i][1]+Environment.NewLine;
+            }
+            return result;
+        }
+
+        public string getRandom()
+        {
+            return "HELOOOFGHRKGHKE";
+        }
+
+
+        
+    }
+}
---
+++ Dialogs/ScoutDialog.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Connector;
+using System.Data;
+using System.Text;
+
+namespace Scout.Dialogs
+{
+    [LuisModel("9f2949ee-4120-4433-a0be-0e633f339718", "c59ef1ca4a0e41e7ad228294d92ae597")]
+    [Serializable]
+    public class ScoutDialog : LuisDialog<object>
+    {
+        [LuisIntent("getIntro")]
+        public async Task getIntro(IDialogContext context, LuisResult result)
+        {
+            string res = "Hi, I am scout and I will help you in finding colleges for your eamcet rank. <br />You can also get the colleges rank information by entering the college name <br />Example: Get the ranks of bvrith<br />";
+            await context.PostAsync(res);
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("getConclu")]
+        public async Task getConclu(IDialogContext context, LuisResult result)
+        {
+            string res = "See you later.Hope I helped you :)";
+            await context.PostAsync(res);
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("getCollege")]
+        public async Task getCollege(IDialogContext context, LuisResult result)
+        {
+            var rankEntity = result.Entities.SingleOrDefault(e => e.Type == "Rank");
+
+            System.Data.DataTable dtExcel;
+            dtExcel = new System.Data.DataTable();
+            dtExcel.TableName = "MyExcelData";
+            string SourceConstr = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source='C:\\Users\\Sathya\\Pictures\\SCOUT-master\\Dialogs\\eamcetdataset.xlsx';Extended Properties= 'Excel 8.0;HDR=Yes;IMEX=1'";
+            OleDbConnection con = new OleDbConnection(SourceConstr);
+            string query = "Select * from [Sheet1$]";
+            OleDbDataAdapter data = new OleDbDataAdapter(query, con);
+            data.Fill(dtExcel);
+
+            string expression = "ocg >= " + Int32.Parse(rankEntity.Entity) + " AND ocg <="+Int32.Parse(rankEntity.Entity)+5000;
+            DataRow[] foundRows;
+
+            // Use the Select method to find all rows matching the filter.
+            foundRows = dtExcel.Select(expression,"ocg ASC");
+
+            //Print column 0 of each returned row.
+
+            int k = 0;
+            string res = "";
+            for (int i = 0; i < foundRows.Length; i++)
+            {
+                k++;
+                res += foundRows[i][0] +  "<br />";
+                if(k == 20)
+                {
+                    break;
+                }
+            }
+
+
+            await context.PostAsync(res);
+            context.Wait(MessageReceived);
+            
+        }
+
+        [LuisIntent("getBestBranch")]
+        public async Task getBestBranch(IDialogContext context, LuisResult result)
+        {
+            var collegeEntity = result.Entities.SingleOrDefault(e => e.Type == "College");
+            System.Data.DataTable dtExcel;
+            dtExcel = new System.Data.DataTable();
+            dtExcel.TableName = "MyExcelData";
+            string SourceConstr = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source='C:\\Users\\Sathya\\Pictures\\SCOUT-master\\Dialogs\\eamcetdataset.xlsx';Extended Properties= 'Excel 8.0;HDR=Yes;IMEX=1'";
+            OleDbConnection con = new OleDbConnection(SourceConstr);
+            string query = "Select * from [Sheet1$]";
+            OleDbDataAdapter data = new OleDbDataAdapter(query, con);
+            data.Fill(dtExcel);
+
+            string expression = "College = '" + collegeEntity.Entity.ToUpper() + "'";
+            DataRow[] foundRows;
+
+            // Use the Select method to find all rows matching the filter.
+            foundRows = dtExcel.Select(expression,"ocg ASC");
+
+            //Print column 0 of each returned row.
+            string res = "";
+                res += foundRows[0][1] + " " + Environment.NewLine;
+          
+
+
+
+            await context.PostAsync(res);
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("getRank")]
+        public async Task getRank(IDialogContext context, LuisResult result)
+        {
+          
+            var collegeEntity = result.Entities.SingleOrDefault(e => e.Type == "College");
+
+            System.Data.DataTable dtExcel;
+            dtExcel = new System.Data.DataTable();
+            dtExcel.TableName = "MyExcelData";
+            string SourceConstr = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source='C:\\Users\\Sathya\\Pictures\\SCOUT-master\\Dialogs\\eamcetdataset.xlsx';Extended Properties= 'Excel 8.0;HDR=Yes;IMEX=1'";
+            OleDbConnection con = new OleDbConnection(SourceConstr);
+            string query = "Select * from [Sheet1$]";
+            OleDbDataAdapter data = new OleDbDataAdapter(query, con);
+            data.Fill(dtExcel);
+
+            string expression = "College = '" + collegeEntity.Entity.ToUpper() + "'";
+            DataRow[] foundRows;
+
+            //Use the Select method to find all rows matching the filter.
+            foundRows = dtExcel.Select(expression);
+
+            //Print column 0 of each returned row.
+            string res = "Branch\t\tOC\t\tSC\t\tST\t\tBC <br />";
+
+            for (int i = 0; i < foundRows.Length; i++)
+            {
+                res += foundRows[i][1] + new string(' ',10) + foundRows[i][3] + new string(' ', 10) + foundRows[i][5] + new string(' ', 10) + foundRows[i][7] + new string(' ', 10) + foundRows[i][9] + "<br />";
+            }
+
+
+            await context.PostAsync(res);
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("getBranches")]
+        public async Task getBranches(IDialogContext context, LuisResult result)
+        {
+
+            var collegeEntity = result.Entities.SingleOrDefault(e => e.Type == "College");
+
+            System.Data.DataTable dtExcel;
+            dtExcel = new System.Data.DataTable();
+                dtExcel.TableName = "MyExcelData";
+                string SourceConstr = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source='C:\\Users\\Sathya\\Pictures\\SCOUT-master\\Dialogs\\eamcetdataset.xlsx';Extended Properties= 'Excel 8.0;HDR=Yes;IMEX=1'";
+                OleDbConnection con = new OleDbConnection(SourceConstr);
+            string query = "Select * from [Sheet1$]";
+                OleDbDataAdapter data = new OleDbDataAdapter(query, con);
+                data.Fill(dtExcel);
+           
+            string  expression = "College = '"+collegeEntity.Entity.ToUpper()+"'";
+            DataRow[] foundRows;
+
+            // Use the Select method to find all rows matching the filter.
+            foundRows = dtExcel.Select(expression);
+
+            //Print column 0 of each returned row.
+            string res = "";
+           
+            for (int i = 0; i < foundRows.Length; i++)
+            {
+                res += foundRows[i][1] + "     "+ "<br />";
+            }
+            
+            await context.PostAsync(res);
+            context.Wait(MessageReceived);
+        }
+        [LuisIntent("getInformation")]
+        public async Task getInformation(IDialogContext context, LuisResult result)
+        {
+
+            var infoEntity = result.Entities.SingleOrDefault(e => e.Type == "College");
+
+            System.Data.DataTable dtExcel;
+            dtExcel = new System.Data.DataTable();
+            dtExcel.TableName = "MyExcelData";
+            string SourceConstr = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source='C:\\Users\\Sathya\\Pictures\\SCOUT-master\\Dialogs\\eamcetdataset.xlsx';Extended Properties= 'Excel 8.0;HDR=Yes;IMEX=1'";
+            OleDbConnection con = new OleDbConnection(SourceConstr);
+            string query = "Select * from [Sheet1$]";
+            OleDbDataAdapter data = new OleDbDataAdapter(query, con);
+            data.Fill(dtExcel);
+
+            string expression = "College = '" + infoEntity.Entity.ToUpper() + "'";
+            DataRow[] foundRows;
+
+            // Use the Select method to find all rows matching the filter.
+            foundRows = dtExcel.Select(expression);
+
+            //Print column 0 of each returned row.
+            string res = "";
+
+
+            res = "To get further information about the college you can visit this website link" + " " + foundRows[0][19] + "     " + "<br />";
+
+
+            await context.PostAsync(res);
+            context.Wait(MessageReceived);
+        }
+
+
+        [LuisIntent("")]
+        [LuisIntent("None")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Sorry I didn't understand that");
+            context.Wait(MessageReceived);
+        }
+    }
+}
